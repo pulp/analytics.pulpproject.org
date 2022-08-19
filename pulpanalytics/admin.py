1755c40086e76ecc28e3c5d656e726f02cf89aa2
Brian Bouterse <bmbouter@gmail.com>
2022-08-19 14:45:20 -0400

    Got data receiving, summarize, and graphs working

--- pulpanalytics/admin.py
+++ pulpanalytics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from pulpanalytics.models import Component, DailySummary, OnlineContentApps, OnlineWorkers, System
+
+
+admin.site.register(Component)
+admin.site.register(DailySummary)
+admin.site.register(OnlineContentApps)
+admin.site.register(OnlineWorkers)
+admin.site.register(System)
--- pulpanalytics/models.py
+++ pulpanalytics/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
-# Create your models here.
+
+class System(models.Model):
+    system_id = models.UUIDField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SystemID={self.id}, Created={self.created}"
+
+
+class Component(models.Model):
+    name = models.TextField()
+    version = models.TextField()
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SystemID={self.id}, Name={self.name}, Version={self.version}"
+
+
+class OnlineContentApps(models.Model):
+    processes = models.IntegerField()
+    hosts = models.IntegerField()
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "online content apps"
+
+    def __str__(self):
+        return f"System={self.system.id}, Processes={self.processes}, Hosts={self.hosts}"
+
+
+class OnlineWorkers(models.Model):
+    processes = models.IntegerField()
+    hosts = models.IntegerField()
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "online workers"
+
+    def __str__(self):
+        return f"System={self.system.id}, Processes={self.processes}, Hosts={self.hosts}"
+
+
+class DailySummary(models.Model):
+    date = models.DateField(unique=True)
+    summary = models.JSONField()
+
+    class Meta:
+        verbose_name_plural = "daily summaries"
+
+    def __str__(self):
+        return f"Summary for {self.date}"
+
+    def epoch_ms_timestamp(self):
+        return int(self.date.strftime('%s')) * 1000
+
+    def online_workers_hosts_avg_data_point(self):
+        return {
+            "x": self.epoch_ms_timestamp(),
+            "y": self.summary["onlineWorkers"]["hostsAvg"]
+        }
+
+    def online_workers_processes_avg_data_point(self):
+        return {
+            "x": self.epoch_ms_timestamp(),
+            "y": self.summary["onlineWorkers"]["processesAvg"]
+        }
+
+    def online_content_apps_hosts_avg_data_point(self):
+        return {
+            "x": self.epoch_ms_timestamp(),
+            "y": self.summary["onlineContentApps"]["hostsAvg"]
+        }
+
+    def online_content_apps_processes_avg_data_point(self):
+        return {
+            "x": self.epoch_ms_timestamp(),
+            "y": self.summary["onlineContentApps"]["processesAvg"]
+        }

ebc26d046216decdefcd5a91a7c7a34805a3a4e9
Matthias Dellweg <mdellweg@redhat.com>
2022-10-28 10:19:10 +0200

    Change test runner to pytest

---
+++ pulpanalytics/tests/test_recieve_telemetry.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from pulpanalytics.telemetry_pb2 import Telemetry
+from pulpanalytics.models import System
+
+
+SYSTEM_ID = "00000000000000000000000000000000"
+
+
+def test_system_reports_twice(db, client):
+    telemetry = Telemetry()
+    telemetry.system_id = SYSTEM_ID
+    response = client.post(reverse("pulpanalytics:index"), telemetry.SerializeToString(), "application/octets")
+    assert response.status_code == 200, response.status_code
+    response = client.post(reverse("pulpanalytics:index"), telemetry.SerializeToString(), "application/octets")
+    assert response.status_code == 200, response.status_code
+    assert System.objects.filter(system_id=SYSTEM_ID).count() == 2

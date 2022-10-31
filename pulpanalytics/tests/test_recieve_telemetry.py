a713e7c20c581e71f9b7e7594dbfe635009324f4
Matthias Dellweg <mdellweg@redhat.com>
2022-10-31 09:47:27 +0100

    Add uniqueness constraint for systems by day
    fixes #37

--- pulpanalytics/tests/test_recieve_telemetry.py
+++ pulpanalytics/tests/test_recieve_telemetry.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from pulpanalytics.telemetry_pb2 import Telemetry
 from pulpanalytics.models import System
@@ -8,11 +9,20 @@ from pulpanalytics.models import System
 SYSTEM_ID = "00000000000000000000000000000000"
 
 
-def test_system_reports_twice(db, client):
+def test_system_reports_twice(monkeypatch, db, client):
     telemetry = Telemetry()
     telemetry.system_id = SYSTEM_ID
+
+    yesterday = timezone.now() - timezone.timedelta(days=1)
+    with monkeypatch.context() as mp:
+        mp.setattr(timezone, "now", lambda : yesterday)
+        response = client.post(reverse("pulpanalytics:index"), telemetry.SerializeToString(), "application/octets")
+    assert response.status_code == 200, response.status_code
+
     response = client.post(reverse("pulpanalytics:index"), telemetry.SerializeToString(), "application/octets")
     assert response.status_code == 200, response.status_code
+
     response = client.post(reverse("pulpanalytics:index"), telemetry.SerializeToString(), "application/octets")
     assert response.status_code == 200, response.status_code
+
     assert System.objects.filter(system_id=SYSTEM_ID).count() == 2
